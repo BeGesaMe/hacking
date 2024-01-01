8b81a27ccb454ed4044d89138a101c9eae9c19e2
BeGesaMe <bekjonshavkatov0@gmail.com>
2024-01-01 05:46:28 +0500

    123

--- app.js
+++ app.js
@@ -18,50 +18,44 @@ function bosing(){
     if(input.value.length == 0){
         alert('Malumot kiriting')
     } else{
-           // Timer boshlash vaqti
+           
 let startTime = Date.now();
 
-// Timer davomiyligi (millisekundlarda)
-let duration = 10000000; // 10 sekund
 
-// Timer tugash vaqti
+let duration = 10000000; 
+
+
 let endTime = startTime + duration;
 
-// Timer holatini ko'rsatadigan element
+
 let timer = document.getElementById("timer");
 
-// Timer funksiyasi
+
 function countdown() {
-  // Hozirgi vaqt
+  
   let now = Date.now();
 
-  // Timer qolgan vaqti
+  
   let remaining = endTime - now;
 
-  // Agar timer tugagan bo'lsa
+  
   if (remaining <= 0) {
-    // Timer funksiyasini to'xtatish
+   
     clearInterval(interval);
 
-    // Timer elementiga "0" yozish
+
     timer.innerHTML = "0";
 
-    // Boshqa ishlar qilish
-    // Masalan, alert chiqarish
-    // alert("Timer tugadi!");
+  
   } else {
-    // Timer elementiga qolgan vaqtni yozish
+   
     timer.innerHTML = Math.ceil(remaining / 1000);
   }
 }
 
-// Timer funksiyasini har sekundda ishga tushirish
-var interval = setInterval(countdown, 1000);
+    let interval = setInterval(countdown, 1000);
 
-    
     }
-    
- 
     
     input.value = '';
 }
